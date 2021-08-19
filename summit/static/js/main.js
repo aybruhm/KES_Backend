a55e9961616b959814b0112011635a9696896b75
Israel Abraham <israelvictory87@gmail.com>
2021-08-19 15:26:48 +0100

    Update static folder

---
+++ summit/static/js/main.js
@@ -0,0 +1,10 @@
+const togBtn = document.getElementById("toggle-button");
+const navTabs = document.getElementById("nav-tabs");
+
+togBtn.addEventListener("click", toggleNavTabs);
+
+function toggleNavTabs(e) {
+    e.preventDefault();
+
+    navTabs.classList.toggle("d-none");
+}

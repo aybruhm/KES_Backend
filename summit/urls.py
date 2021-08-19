de411f9cbb8f00803bc0697a0fb618c1afa90c30
Israel Abraham <israelvictory87@gmail.com>
2021-08-19 15:55:56 +0100

    Created summit_page and send_message view

--- summit/urls.py
+++ summit/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from summit import views
 
+app_name = "summit"
 
 urlpatterns = [
+    path("", views.summit_page, name="summit_page"),
+    path("submit-message/", views.submit_message, name="submit_message")
 ]
--- summit/views.py
+++ summit/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, redirect
 
-# Create your views here.
+
+def summit_page(request):
+    return render(request, "summit/index.html")
+
+
+def submit_message(request):
+    name = request.POST.get("name")
+    email = request.POST.get("email")
+    mobile_number = request.POST.get("mobile_number")
+    about_you = request.POST.get("describe")
+
+    print(name)
+    print(email)
+    print(mobile_number)
+    print(about_you)
+
+    return redirect("summit:summit_page")

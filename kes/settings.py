ef413ecbfa74f3f504e4b4c41d2b2fcb386a876a
Israel Abraham <israelvictory87@gmail.com>
2021-08-20 22:49:57 +0100

    Update requirements.txt

--- kes/settings.py
+++ kes/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'summit.apps.SummitConfig'
+    'summit.apps.SummitConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [

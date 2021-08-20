9e3e9ad52849811101fb41cba076f0d6108f4f1b
Israel Abraham <israelvictory87@gmail.com>
2021-08-20 23:19:42 +0100

    Update blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 
-def single_post(request: HttpRequest) -> HttpResponse:
+def single_post(request: HttpRequest, slug: str) -> HttpResponse:
 
     context = {}
 
     return render(request, "blog/blog-single.html", context)
 
 
-def posts(request: HttpRequest, slug: str) -> HttpResponse:
+def posts(request: HttpRequest) -> HttpResponse:
 
     context = {}
 
